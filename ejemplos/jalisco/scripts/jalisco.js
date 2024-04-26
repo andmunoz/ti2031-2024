8702021d1164fa292f9a229f5ab636e31c99f4c9
Andrés Muñoz Órdenes <andmunoz@gmail.com>
2024-04-25 21:05:58 -0400

    jalisco v2

--- ejemplos/jalisco/scripts/jalisco.js
+++ ejemplos/jalisco/scripts/jalisco.js
@@ -1,12 +1,14 @@
 function jalisco() {
     let n = document.getElementById('numero').value;
     n = parseInt(n);
-    console.log('El valor del usuario es ' + n);
+    console.log('Número jugado por el usuario: ' + n);
     if (isNaN(n)) {
         window.alert('Debes ingresar un número');
-    } else if (n >= 1 && n <=100) {
-        document.getElementById('respuesta').innerHTML = 'Te gané con el <b>' + (n + 1) + '</b>. Ja ja ja'
+    } else if (n >= 1 && n <= 100) {
+        let n1 = n + 1;
+        console.log('Mí número a jugar: ' + n1);
+        document.getElementById('respuesta').innerText = 'Ja ja ja. Te gané con el ' + n1;
     } else {
-        window.alert('Eres un mardeto tramposo!');
+        window.alert('Eres un maldito tramposo!!!');
     }
 }

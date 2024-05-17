362134ea6570bb59ddf8e7dae930db23447c66f8
Andrés Muñoz Órdenes <andmunoz@gmail.com>
2024-05-16 21:04:46 -0400

    object example javascript code

--- ejemplos/character_sheet/scripts/script.js
+++ ejemplos/character_sheet/scripts/script.js
@@ -1,1 +1,20 @@
 // Aquí haremos el script de Javascript
+let character = {};
+
+function agregarAtributo(idNombre, idValor) {
+    // Obtener los valores del nombre y contenido del atributo
+    let atributoNombre = document.getElementById(idNombre).value;
+    let atributoValor = document.getElementById(idValor).value;
+    console.log(atributoNombre + ' = ' + atributoValor);
+
+    // Agregar la información al objeto character
+    character[atributoNombre] = atributoValor;
+    console.log(character);
+
+    // Motrar el nuevo atributo en la tabla de datos
+    let body = document.getElementById('body');
+    body.innerHTML = '';
+    for(atrib in character) {
+        body.innerHTML += '<tr><td>' + atrib + '</td><td>' + character[atrib] + '</td></tr>';
+    }
+}

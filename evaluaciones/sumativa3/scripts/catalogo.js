e39cf2b624d3df940e63a3ea533e42268e64fb87
Andrés Muñoz O <andmunoz@moit.cl>
2024-05-27 01:36:18 -0400

    product catalog  for sumativa2

---
+++ evaluaciones/sumativa3/scripts/catalogo.js
@@ -0,0 +1,32 @@
+let catalogo = [
+    { 
+        codigo: 'P0001', 
+        categoria: 'Juegos de Tablero',
+        nombre: 'Tablero de Damas',
+        descripcion: 'Tablero para el juego de damas. Incluye un tablero y 24 fichas repartidas entre el color blanco y negro. Tablero se convierte en maleta para guardar las fichas.',
+        ubicacion: 'Renca',
+        imagen: 'p0001.png',
+        disponibilidad: 10,
+        tiempo: 5
+    },
+    { 
+        codigo: 'P0002', 
+        categoria: 'Juegos de Tablero',
+        nombre: 'Tablero de Ajedrez',
+        descripcion: 'Tablero para jugar Ajedrez. Incluye un tablero y con el set de fichas completo tanto de color blanco como negro. Tablero se convierte en maleta para guardar las fichas.',
+        ubicacion: 'Renca',
+        imagen: 'p0002.jpg',
+        disponibilidad: 10,
+        tiempo: 5
+    },
+    { 
+        codigo: 'P0003', 
+        categoria: 'Juegos de Rol',
+        nombre: 'Dungeons & Dragons 5ed, Manual del Jugador',
+        descripcion: 'Manual del jugador para la 5ta edición del juego de rol Dungeons & Dragons. Tapa dura y en español.',
+        ubicacion: 'Santiago Centro',
+        imagen: 'p0003.jpg',
+        disponibilidad: 3,
+        tiempo: 5
+    },
+];

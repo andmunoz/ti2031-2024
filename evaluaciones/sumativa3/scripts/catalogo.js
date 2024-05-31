38c01442cd02963e9e1ad0466b6e755c72f0efc3
Andrés Muñoz Órdenes <andmunoz@gmail.com>
2024-05-30 20:06:38 -0400

    add some games to database.

--- evaluaciones/sumativa3/scripts/catalogo.js
+++ evaluaciones/sumativa3/scripts/catalogo.js
@@ -31,12 +31,72 @@ let catalogo = [
     },
     { 
         codigo: 'P0004', 
-        categoria: 'Juegos de Tablero',
+        categoria: 'Juegos de Estrategia',
         nombre: 'Scythe',
         descripcion: 'Ambientado en una realidad paralela de 1920, Scythe es un juego de estrategia tipo 4x para 1 a 5 jugadores (ampliado a 7 jugadores en la expansión “Invasores de tierras lejanas“).',
         ubicacion: 'Renca',
         imagen: 'p0004.png',
         disponibilidad: 7,
+        tiempo: 3
+    },
+    { 
+        codigo: 'P0005', 
+        categoria: 'Juego Creativo',
+        nombre: 'Dixit',
+        descripcion: '¡Dixit es un creativo juego de deducción, bellamente ilustrado, donde tu imaginación crea increíbles historias! 84 cartas con bellas ilustraciones para que los adultos se luzcan con su agudeza y los pequeños sorprendan con su imaginación. ',
+        ubicacion: 'Santiago Centro',
+        imagen: 'p0005.png',
+        disponibilidad: 12,
+        tiempo: 5
+    },
+    { 
+        codigo: 'P0006', 
+        categoria: 'Juegos Competitivo',
+        nombre: 'Aventureros al Tren (Ticket to Ride)',
+        descripcion: '¡Aventureros al Tren! es un juego de aventuras en tren. Los jugadores acumulan cartas de ciertos tipos de vagones que les permiten conectar diferentes ciudades a lo largo y ancho de Norteamérica. Mientras más largos son los recorridos, más puntos se ganan.',
+        ubicacion: 'Santiago Centro',
+        imagen: 'p0006.png',
+        disponibilidad: 10,
+        tiempo: 5
+    },
+    { 
+        codigo: 'P0007', 
+        categoria: 'Juegos de Cartas',
+        nombre: 'Dobble',
+        descripcion: 'Dobble un juego con más de 50 símbolos, 55 cartas, con uno, y solamente un símbolo idéntico entre cada carta. ¿Serás capaz de descubrirlo?',
+        ubicacion: 'Renca',
+        imagen: 'p0007.png',
+        disponibilidad: 7,
+        tiempo: 3
+    },
+    { 
+        codigo: 'P0008', 
+        categoria: 'Juegos de Estrategia',
+        nombre: 'Catan, El Juego',
+        descripcion: 'Catan es un juego de mesa para toda la familia que se ha convertido en un fenómeno mundial. Desde su aparición en Alemania ha vendido más que muchos de los juegos más tradicionales. Se trata de un juego que aúna la estrategia, la astucia y la capacidad para negociar y en el que los jugadores tratan de colonizar una isla, Catán, rica en recursos naturales.',
+        ubicacion: 'Santiago Centro',
+        imagen: 'p0008.png',
+        disponibilidad: 2,
+        tiempo: 5
+    },
+    { 
+        codigo: 'P0009', 
+        categoria: 'Juegos Cooperativos',
+        nombre: 'Arkham Horror',
+        descripcion: 'Arkham Horror es un juego cooperativo. Todos los jugadores colaboran entre sí y ganan o pierden la partida como equipo. Cada jugador controla a un intrépido investigador, uno de los pocos individuos excepcionales que están al tanto del peligro que se cierne sobre nuestro mundo. Los investigadores deben explorar la ciudad para descubrir lugares, personas y criaturas de naturaleza tanto ordinaria como sobrenatural.',
+        ubicacion: 'Renca',
+        imagen: 'p0009.png',
+        disponibilidad: 6,
+        tiempo: 3
+    },
+    { 
+        codigo: 'P0010', 
+        categoria: 'Juegos de Cartas',
+        nombre: 'Timeline - Eventos',
+        descripcion: '¿Acaso Julio César había sido nombrado dictador cuando ocurrió el Gran Incendio de Roma? ¿Había comenzado ya la construcción de la Gran Muralla China? ¿La Fiebre del Oro de California tuvo lugar al mismo tiempo? Para ganar la partida, sé el primero en colocar correctamente todas tus cartas.',
+        ubicacion: 'Renca',
+        imagen: 'p0010.png',
+        disponibilidad: 15,
         tiempo: 3
     },
 ];

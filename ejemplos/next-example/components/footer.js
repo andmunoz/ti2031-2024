ba672c2399193ec1ec08ab005d55ed57425c5e3b
Andrés Muñoz O <andmunoz@moit.cl>
2024-07-04 04:07:23 -0400

    next-example

---
+++ ejemplos/next-example/components/footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <main>
+            <div className="mb-32 grid text-center lg:max-w-12xl lg:w-full lg:mb-0 lg:grid-cols-1 lg:text-center">
+                Creado por Andrés Muñoz O. 2024
+            </div>
+        </main>
+    )
+  }
